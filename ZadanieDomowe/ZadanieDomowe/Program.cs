1667299b405172af68ff922046913e64c7869ba9
Marcin Siry <sirymarcin@gmail.com>
2023-12-18 20:28:54 +0000

    Zadanie domowe dzień 4

---
+++ ZadanieDomowe/ZadanieDomowe/Program.cs
@@ -0,0 +1,22 @@
+﻿var name = "Ewa";
+bool female = true;
+var age = 30;
+if (female)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (name == "Ewa" && age == 30)
+    {
+        Console.WriteLine("Ewa, lat 30");
+    }
+
+}
+if (!female && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+}
